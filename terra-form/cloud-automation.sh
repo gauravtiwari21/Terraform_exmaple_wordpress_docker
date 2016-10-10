d3c7291cf62a7a4e9b809f39eae142240ff92f2f
Gaurav Tiwari <gtiwari@atlassian.com>
2016-10-10 16:50:07 +1100

    Print public access URL

--- terra-form/cloud-automation.sh
+++ terra-form/cloud-automation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+# TODO Add named arg parsing and error handling
+
 APP="$1"
 ENV="$2"
 COUNT="$3"
@@ -10,6 +12,10 @@ terraform apply -var "access_key=${AWS_ACCESS_KEY}" -var "secret_key=${AWS_SECRE
 
 # PREPARE VARS TO RUN ANSIBLE PLAYBOOK
 export terraform_inventory=$(which terraform-inventory)
+export PUBLIC_ACCESS_POINT=$(terraform output | grep App_entrypoint_address | cut -d = -f2)
 export DB_URL=$(cat terraform.tfstate | grep endpoint | cut -d : -f2- | tr -d '"' | tr -d , | tr -d ' ')  # This is the RDS acess endpoint 
 export ansible_params="DB_HOST=${DB_URL} DB_USER=${DB_USERNAME} -var DB_PASS=${DB_PASSWORD}"    # These variables are required to set as environment in running wordpress docker
 TF_STATE=terraform.tfstate ansible-playbook --inventory-file=${terraform_inventory} ../ansible/deploy_docker.yml --extra-vars "${ansible_params}" -v
+
+# Print the public access URL
+echo "Public access URL to access wordpress is ${PUBLIC_ACCESS_POINT}"
