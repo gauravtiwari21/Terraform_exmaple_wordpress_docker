ae1157b51c7941a6be791e77141c9390c1fee813
Gaurav Tiwari <gtiwari@atlassian.com>
2016-10-15 17:24:34 +1100

    Fix the string variable passing for count

--- terra-form/cloud-automation.sh
+++ terra-form/cloud-automation.sh
@@ -8,7 +8,9 @@ COUNT="$3"
 SIZE="$4"
 region="us-east-1"
 
-terraform apply -var "access_key=${AWS_ACCESS_KEY}" -var "secret_key=${AWS_SECRET}" -var 'region=us-east-1' -var "aws_key_pair_path=${AWS_KEY_PATH}" -var "aws_key_pair_name=${AWS_KEY_PAIR_NAME}" -var "db_username=${DB_USERNAME}" -var "db_password=${DB_PASSWORD}" -var "app_name=${APP}" -var "environment=${ENV}" -var "instance_type=${SIZE}" -var "count=${COUNT}"
+export TF_VAR_COUNT=$3
+
+terraform apply -var "access_key=${AWS_ACCESS_KEY}" -var "secret_key=${AWS_SECRET}" -var 'region=us-east-1' -var "aws_key_pair_path=${AWS_KEY_PATH}" -var "aws_key_pair_name=${AWS_KEY_PAIR_NAME}" -var "db_username=${DB_USERNAME}" -var "db_password=${DB_PASSWORD}" -var "app_name=${APP}" -var "environment=${ENV}" -var "instance_type=${SIZE}" 
 
 # PREPARE VARS TO RUN ANSIBLE PLAYBOOK
 export terraform_inventory=$(which terraform-inventory)
